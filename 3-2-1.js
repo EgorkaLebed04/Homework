c548106b06b7f74e49686289bb1bfa40f3a9c7cf
EgorkaLebed04 <egorka04050806@gmail.com>
2026-07-10 18:14:43 +0700

    add 322 edit 321

--- 3-2-1.js
+++ 3-2-1.js
@@ -1,12 +1,19 @@
 function getReputation(likes, dislikes, editMessages, allMessages) {
+    let res;
     if (likes > dislikes) {
         if (allMessages > 0) {
-            let res = (likes - dislikes) * ((allMessages - editMessages) / allMessages)
+            res = (likes - dislikes) * ((allMessages - editMessages) / allMessages)
         } else res = 0;
     } else {
-        res = likes - dislikes
+        res = likes - dislikes;
     }
-    return res
+    return res;
 }
 
-console.log(getReputation(10, 5, 5, 10));
+console.log(getReputation(10, 5, 10, 10));
+
+// В задании указано "Коэффициент — доля не отредактированных сообщений пользователя.", так получается,
+// если все сообщения отредактированы, коэффицент = 0. 
+
+// если нужно наоборот, тогда формула будет выглядеть:
+//     res = (likes - dislikes) * (editMessages / allMessages)
